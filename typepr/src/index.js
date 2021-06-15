e9d21262507bd4dca306d9ca0e1a1e85bb48d5c2
1 <a1@1ui-MacBookPro.local>
2021-06-15 19:05:08 +0900

    structure

---
+++ typepr/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var value = rand();
+var str = value;
+console.log(str * 10);
+function rand() {
+    return "wowo";
+}
---
+++ typepr/src/index.ts
@@ -0,0 +1,11 @@
+const value = rand();
+
+const str = value as number;
+console.log(str * 10);
+
+function rand(): string | number {
+		return "wowo";
+
+}
+
+export {}
